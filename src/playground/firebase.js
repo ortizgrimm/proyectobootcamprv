0d800805996edad7e003dec05eba8b34518141d2
ortizgrimm <danilobgomez@gmail.com>
2025-11-27 11:01:12 -0500

    creacion de firebase

---
+++ src/playground/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBmW8bkJwFQsTOCNhb8-Qu4FDS768oN-vE",
+  authDomain: "proyecboot.firebaseapp.com",
+  projectId: "proyecboot",
+  storageBucket: "proyecboot.firebasestorage.app",
+  messagingSenderId: "647212471780",
+  appId: "1:647212471780:web:6a2066560bd7dde954e3f1"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
